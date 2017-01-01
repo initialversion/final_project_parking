a7d1e56831223c36c171bbed10e49728a4e3b9c6
Murugan <murugu2012@gmail.com>
2017-01-01 18:17:53 +0530

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "private_parkings#index"
   # Routes for the Private_parking resource:
   # CREATE
